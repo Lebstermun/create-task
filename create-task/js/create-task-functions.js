4ffe2dc0816899a70085edf434b4333805241e0d
Will Harder <wharder@ucvts.org>
2019-03-13 13:18:19 -0400

    finished paths

--- create-task/js/create-task-functions.js
+++ create-task/js/create-task-functions.js
@@ -7,9 +7,6 @@ function choicetwo() {
 function choiceDeath() {
   window.location='death-page.html';
 }
-function choiceThree() {
-  window.location='choiceThree.html';
-}
 function choiceReturn() {
     window.location='create-task.html'
 }
@@ -21,4 +18,67 @@ function choiceWin() {
 }
 function bankPlane() {
   window.location='bankerandPlane.html'
+}
+function anthroNative() {
+  window.location='anthroNative.html'
+}
+function BankTemple() {
+  window.location='BankTemple.html'
+}
+function bridge(){
+  window.location='bridge.html'
+}
+function goldenStairs(){
+  window.location='deathInGoldRoom.html'
+}
+function dropNative(){
+  window.location='dropNative.html'
+}
+function fightRivial(){
+  window.location='fightRivial.html'
+}
+function followAnthro(){
+  window.location='followAnthro.html'
+}
+function followNative(){
+  window.location='followNative.html'
+}
+function hide(){
+  window.location='hide.html'
+}
+function idolRoom(){
+  window.location='idolRoom.html'
+}
+function look(){
+  window.location='look.html'
+}
+function quicksandDeath(){
+  window.location='quicksandDeath.html'
+}
+function saveBank(){
+  window.location='savebank.html'
+}
+function saveIdol(){
+  window.location='saveIdol.html'
+}
+function saveNativeWin(){
+  window.location='SaveNativeWin.html'
+}
+function saveplane(){
+  window.location='saveplane.html'
+}
+function saveRivialOne(){
+  window.location='saveRivial.html'
+}
+function saveRivialTwo(){
+  window.location='saveRivialOne.html'
+}
+function saveNativeBridge(){
+  window.location='saveNativeBridge.html'
+}
+function enterTemple(){
+  window.location='Temple.html'
+}
+function shotInTheBack(){
+  window.location='shotInBack.html'
 }
